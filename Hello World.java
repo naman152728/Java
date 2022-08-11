8c76e978e94b83219ae7ad78963cdc4d720dbd32
naman152728 <84771191+naman152728@users.noreply.github.com>
2022-08-11 09:44:22 +0530

    Create Hello World.java

---
+++ Hello World.java
@@ -0,0 +1,8 @@
+public class JavaBasics
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello Naman!!");
+        System.out.println("How's going on?");
+    }
+}
